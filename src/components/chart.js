3b359d31c12042d8587de2bcb71c7d26cf074fa2
gongbaochicken <jiazhuo0528@yahoo.com>
2016-12-14 22:13:24 -0600

    add chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import React from 'react';
 import {Sparklines, SparklinesLine} from 'react-sparklines';
 
 export default (props) => {
   return (
-    <div>
+    <div className="D3Chart">
       <Sparklines heigth={50} width={60} data={props.data}>
         <SparklinesLine color= {props.color} />
       </Sparklines>
