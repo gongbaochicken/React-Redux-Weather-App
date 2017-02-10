b872a21e670f1056f29607495125d24be8407b87
gongbaochicken <jiazhuo0528@yahoo.com>
2017-02-09 23:57:28 -0600

    add description

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,13 +4,21 @@ import Chart from '../components/chart';
 
 class WeatherList extends Component{
   renderWeather(cityData){
+    console.log(cityData);
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const description = cityData.list[0].weather[0].description;
+    console.log(description);
 
     return(
       <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+        <td>
+          {cityData.city.name}
+        </td>
+        <td>
+          {description}
+        </td>
         <td><Chart data={temps} color="orange" /></td>
         <td><Chart data={pressures} color="red" /></td>
         <td><Chart data={humidities} color="purple" /></td>
@@ -23,6 +31,7 @@ class WeatherList extends Component{
       <table className="table table-hover">
         <tr>
           <th>City</th>
+          <th>Description</th>
           <th>Temperature</th>
           <th>Pressure</th>
           <th>Humidity</th>
